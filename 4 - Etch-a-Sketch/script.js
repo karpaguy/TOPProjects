5c1d75ad32b2c6bc2751f89cbd2aad15ac4562ab
karpaguy <denuzzidouglas@gmail.com>
2024-12-27 15:23:33 -0300

    Better CSS

--- 4 - Etch-a-Sketch/script.js
+++ 4 - Etch-a-Sketch/script.js
@@ -1,2 +1,38 @@
-// Width → 512/n; Height → 512/n
+// const sizeInput = document.querySelector('.size-input');
+// const submitBtn = document.querySelector('#submit');
+
+// function createGrid(){
+//     const grid = document.createElement('div');
+//     grid.classList.add('grid');
+
+// }
+
+// function changeGridSize() {
+//     for (let i = 0; i <= sizeInput.value; i++) {
+//         const colorBlock = document.createElement('div');
+//         colorBlock.classList.add('color-block');
+//         colorBlock.style.width = `${512 / sizeInput.value}px`;
+//         colorBlock.style.height = `${512 / sizeInput.value}px`;
+//         grid.append(colorBlock);
+
+//         // Essa lógica atual está errada.
+//     }
+//     // 
+//     // Remeber to add catchs for possible errors, like adding a text instead of a number in the input.
+    
+// };
+
+
+// submitBtn.addEventListener('click', changeGridSize);
+
+
+// // ---------
+// // const colorBlock = document.createElement('div');
+// // colorBlock.classList.add('color-block');
+// // grid.append(colorBlock);
+
+// // Width → 512/n; Height → 512/n
+
+// // Map com base em n, com o Width e Height definido na conta gridSize/n.
+// // n sendo o total de blocos que o usuário deseja, com um limite de 100.
 
