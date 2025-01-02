833180be13138befff04e1768f2fcdcdcc95a46d
karpaguy <denuzzidouglas@gmail.com>
2025-01-02 20:39:28 -0300

    Color working, but its not reseting older grid

--- 4 - Etch-a-Sketch/script.js
+++ 4 - Etch-a-Sketch/script.js
@@ -1,9 +1,9 @@
-// const sizeInput = document.querySelector('.size-input');
-// const submitBtn = document.querySelector('#submit');
+;
+const submitBtn = document.querySelector('#submit');
 const gridContainer = document.querySelector('.grid-container');
+const sizeInput = document.querySelector('.size-input')
 
-
-function createGrid(sizeDesired){
+function setGrid(sizeDesired){
     let squareWidth = 512 / sizeDesired;
     let squareMaxCount = sizeDesired ** 2;
 
@@ -11,9 +11,38 @@ function createGrid(sizeDesired){
         const square = document.createElement('div');
         square.classList.add('color-square');
         square.style.width = squareWidth + "px";
+        square.addEventListener('mouseover', paintBlack);
+
         gridContainer.appendChild(square);
     }
 }
+
+function paintBlack(e) {
+    this.style.backgroundColor = 'black';
+}
+
+function setNewGrid() {
+    let newGridSize = sizeInput.value;
+    setGrid(newGridSize)
+}
+
+setGrid(4);
+submitBtn.addEventListener('click', setNewGrid);
+
+/* 
+setNewGrid() {
+    let newSize = Input do User
+
+    confere se tá ok
+        se ok {
+            setGrid
+        }
+        se não {
+            fala que tá torto
+        }
+}
+
+*/
 
 // function changeGridSize() {
 //     for (let i = 0; i <= sizeInput.value; i++) {
@@ -23,10 +52,6 @@ function createGrid(sizeDesired){
 //         grid.append(colorBlock);
 //     }    
 // };
-
-createGrid(4);
-// submitBtn.addEventListener('click', changeGridSize);
-
 
 // // ---------
 // // const colorBlock = document.createElement('div');
