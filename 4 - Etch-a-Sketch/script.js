c2703fa9664e0b314f9508ae809868988ef3f194
karpaguy <denuzzidouglas@gmail.com>
2025-01-03 20:43:59 -0300

    Grid now has size limit

--- 4 - Etch-a-Sketch/script.js
+++ 4 - Etch-a-Sketch/script.js
@@ -12,54 +12,26 @@ function setGrid(sizeDesired){
         square.classList.add('color-square');
         square.style.width = squareWidth + "px";
         square.addEventListener('mouseover', paintBlack);
-
         gridContainer.appendChild(square);
     }
 }
 
-function paintBlack(e) {
+function paintBlack() {
     this.style.backgroundColor = 'black';
 }
 
 function setNewGrid() {
     let newGridSize = sizeInput.value;
+
+    if (newGridSize > 100) {
+        alert('Grid Size cant be higher than 100x100!');
+        return;
+    };
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild)
+    };
     setGrid(newGridSize)
 }
 
 setGrid(4);
 submitBtn.addEventListener('click', setNewGrid);
-
-/* 
-setNewGrid() {
-    let newSize = Input do User
-
-    confere se tá ok
-        se ok {
-            setGrid
-        }
-        se não {
-            fala que tá torto
-        }
-}
-
-*/
-
-// function changeGridSize() {
-//     for (let i = 0; i <= sizeInput.value; i++) {
-//         const colorBlock = document.createElement('div');
-//         colorBlock.classList.add('color-block');
-//         colorBlock.style.width = `${(512 / sizeInput.value) * 2}px`;
-//         grid.append(colorBlock);
-//     }    
-// };
-
-// // ---------
-// // const colorBlock = document.createElement('div');
-// // colorBlock.classList.add('color-block');
-// // grid.append(colorBlock);
-
-// // Width → 512/n; Height → 512/n
-
-// // Map com base em n, com o Width e Height definido na conta gridSize/n.
-// // n sendo o total de blocos que o usuário deseja, com um limite de 100.
-
