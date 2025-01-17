aa5606349f1f602808c295e7f0af76f5c22589e7
karpaguy <denuzzidouglas@gmail.com>
2025-01-17 20:33:29 -0300

    fibo and titles

--- 4.1 - Object Basics/10_fibonacci/fibonacci.js
+++ 4.1 - Object Basics/10_fibonacci/fibonacci.js
@@ -1,7 +1,18 @@
-const fibonacci = function() {
-    
+const fibonacci = function(positionDesired) {
+    // if (positionDesired.isInteger() != true) positionDesired += positionDesired;
+    if (positionDesired == 0) return 0;
+    if (positionDesired < 0) return "OOPS";
 
-    // 01 soma esses dois e soma seguinte
+    let previosNum = 0;   
+    let currentNum = 1;
+
+    for (let i = 2; i <= positionDesired; i++) {
+        let accumulator = previosNum + currentNum;
+        previosNum = currentNum;
+        currentNum = accumulator;
+    }
+
+    return currentNum;
 };
 
 // Do not edit below this line
--- 4.1 - Object Basics/11_getTheTitles/getTheTitles.js
+++ 4.1 - Object Basics/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const booksTitle = books.map( (book) => {
+        return book[Object.keys(book)[0]];
 
+        // I've could used Object.values too.
+    });
+
+    return booksTitle;
 };
 
 // Do not edit below this line
