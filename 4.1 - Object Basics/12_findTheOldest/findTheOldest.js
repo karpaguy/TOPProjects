a0d1243b1cf696765273304c63f9d8bccfedb063
karpaguy <denuzzidouglas@gmail.com>
2025-01-27 11:15:49 -0300

    All exercises done!

--- 4.1 - Object Basics/12_findTheOldest/findTheOldest.js
+++ 4.1 - Object Basics/12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    people.forEach(person => {
+        if(!("yearOfDeath" in person)) {
+          const yearNow = new Date();
+          person.yearOfDeath = yearNow.getFullYear();
+        }
+      })
+
+    const peopleOld = people.sort( (personA, personB) => {
+        return (personA[Object.keys(personA)[2]] - personA[Object.keys(personA)[1]]) < (personB[Object.keys(personB)[2]] - personB[Object.keys(personB)[1]]) ? 1 : -1;      
+     })
+
+     const oldestPerson = peopleOld[0];
+
+     return oldestPerson;
 
 };
 
